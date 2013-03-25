b31dd890019d529aa759363c095579a445a91b3d
Brendan O'Connor <brendan@maliceafterthought.com>
2013-03-25 14:28:32 -0700

    Tor now in config. Can send SIGUSR1 to overall.sh to make it reload the programs.

--- overall.sh
+++ overall.sh
@@ -2,11 +2,11 @@
 
 PREFIX=/tmp/crypt
  
-cleanup()
+shutdown()
 {
-  #rm -f /tmp/tempfile
-  PREFIX=$PREFIX; couchdb -d -n -a $PREFIX/conf/couchdb.ini -p $PREFIX/working/couchdb/couch.pid
+  PREFIX=$PREFIX; kill -s SIGINT `cat $PREFIX/working/tor/tor.pid`
   PREFIX=$PREFIX; nginx -p $PREFIX/working/nginx/ -c $PREFIX/conf/nginx.conf -s quit
+  PREFIX=$PREFIX; couchdb -d -n -a $PREFIX/conf/couchdb.ini -p $PREFIX/working/couchdb/couch.pid
   return $?
 }
  
@@ -14,32 +14,49 @@ control_c()
 # run if user hits control-c
 {
   echo -en "\n*** Exiting ***\n"
-  cleanup
+  shutdown
   exit $?
+}
+
+startup()
+{
+    #-b: Run in background
+    #-n: Reset configuration chain (to get rid of /etc)
+    #-a: Use this configuration file
+    #-p: Use this PID file
+    #-d: Shutdown the system
+    #-o: Use this STDOUT file
+    #-e: Use this STDERR file
+
+    PREFIX=$PREFIX; couchdb -b -n -a $PREFIX/conf/couchdb.ini \
+        -p $PREFIX/working/couchdb/couch.pid \
+        -o $PREFIX/working/couchdb/couch.stdout \
+        -e $PREFIX/working/couchdb/couch.stderr 
+
+
+    #-p: Set prefix (to make relative paths work in configuration file)
+    #-c: Set configuration file
+    #-s: Send signal (e.g., quit)
+    PREFIX=$PREFIX; nginx -p $PREFIX/working/nginx/ -c $PREFIX/conf/nginx.conf
+
+    #-f: Use this configuration file
+    PREFIX=$PREFIX; tor -f $PREFIX/conf/torrc
+}
+
+restart()
+{
+    shutdown
+    startup
 }
  
 # trap keyboard interrupt (control-c)
 trap control_c SIGINT
- 
 
-#-b: Run in background
-#-n: Reset configuration chain (to get rid of /etc)
-#-a: Use this configuration file
-#-p: Use this PID file
-#-d: Shutdown the system
-#-o: Use this STDOUT file
-#-e: Use this STDERR file
-
-PREFIX=$PREFIX; couchdb -b -n -a $PREFIX/conf/couchdb.ini \
-    -p $PREFIX/working/couchdb/couch.pid \
-    -o $PREFIX/working/couchdb/couch.stdout \
-    -e $PREFIX/working/couchdb/couch.stderr 
+trap restart SIGUSR1
 
 
-#-p: Set prefix (to make relative paths work in configuration file)
-#-c: Set configuration file
-#-s: Send signal (e.g., quit)
-PREFIX=$PREFIX; nginx -p $PREFIX/working/nginx/ -c $PREFIX/conf/nginx.conf
+startup
  
 # main() loop
 while true; do sleep 1; done
+
